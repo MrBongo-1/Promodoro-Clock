751089e2c534b230e00805dbb5c7b7bb7cec8816
Angelo Pool Rodriguez <brofan66@gmail.com>
2020-12-28 20:06:00 -0500

    Updates

---
+++ precache-manifest.b39c437bffb2552dc363fe21113f1ed5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "535624bc712973b385b9cc9934438c1c",
+    "url": "/Promodoro-Clock/index.html"
+  },
+  {
+    "revision": "b0d90e1e0fe82b0b586a",
+    "url": "/Promodoro-Clock/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "b4fd47a39741ba908a94",
+    "url": "/Promodoro-Clock/static/js/2.93dea4d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Promodoro-Clock/static/js/2.93dea4d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0d90e1e0fe82b0b586a",
+    "url": "/Promodoro-Clock/static/js/main.98f2579b.chunk.js"
+  },
+  {
+    "revision": "98149a801e7de7006717",
+    "url": "/Promodoro-Clock/static/js/runtime-main.599d325c.js"
+  },
+  {
+    "revision": "b01f097888bd7b5b219ac40ac68ccfe7",
+    "url": "/Promodoro-Clock/static/media/Beep.b01f0978.mp3"
+  }
+]);
